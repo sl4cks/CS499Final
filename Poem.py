4107dae6a22ccbd71c5e492635a836f3b33306be
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-04-30 18:27:25 -0400

    made __init__ methods

--- Poem.py
+++ Poem.py
@@ -1,35 +1,35 @@
 class Poem:
 
     def __init__(self, poem_path=None):
-        if (poem_path != None):
+        if poem_path is None:
             file = open(poem_path, "r")
-            self._lines = file.readlines()
+            self._lines = []  # create empty list
+            for line in file.readlines():
+                self._lines.appendA(self.Line(line))  # might need to be extend instead of append
 
     def print_lines(self):
         print(self._lines)
 
     def read_poem(self, poem_path):
         file = open(poem_path, "r")
-        self._lines = file.readlines()
+        self._lines = [] # create empty list
+        for line in file.readlines():
+            self._lines.appendA(self.Line(line)) # might need to be extend instead of append
 
     def get_line(self, line_number):
         return self._lines[line_number]
 
-    def __iter__(self):
-        return self._lines.__iter__()
-
-    def __next__(self):
-        return self._lines.__next__()
-
     class Line:
 
-        def __init__(self):
-            self.line = "test" #filler
+        def __init__(self, line):
+            self._words = []
+            for word in line.split():
+                self._words.append(self.Word(word))
 
         class Word:
 
-            def __init__(self):
-                self.word = "word" #filler
+            def __init__(self, word):
+                self.word = word
 
 
 def __main__():
@@ -37,5 +37,5 @@ def __main__():
     print(poem.get_line(0))
 
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     __main__()
