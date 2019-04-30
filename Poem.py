f487f7678de43f82366d7ff401cb8f7d488207b4
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-04-30 16:53:19 -0400

    reading poems

---
+++ Poem.py
@@ -0,0 +1,22 @@
+
+class Poem:
+
+    def __init__(self, poem_path = None):
+        if(poem_path != None):
+            file = open(poem_path, "r")
+            self._lines = file.readlines()
+
+    def print_lines(self):
+        print(self._lines)
+
+    def read_poem(self, poem_path):
+        file = open(poem_path, "r")
+        self._lines = file.readlines()
+
+
+def __main__():
+    poem = Poem("poem.txt")
+    poem.print_lines()
+
+if(__name__ == "__main__"):
+    __main__()
