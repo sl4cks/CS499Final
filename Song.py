25c14a277b5f4799c28b4388d8ccd13b7c800a4b
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-05-05 14:36:41 -0400

    created inits

---
+++ Song.py
@@ -0,0 +1,15 @@
+class Song:
+
+    def __init__(self):
+        self.Note = self.Note()
+
+    class Note:
+
+        def __init__(self):
+            self.Rhythm = self.Rhythm()
+            self.pitch = 0
+
+        class Rhythm:
+
+            def __init__(self):
+                self.duration = 0
