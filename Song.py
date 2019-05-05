369bf17b854d4c6c76c12e47b7ef75a56aa46a2c
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-05-05 16:15:22 -0400

    input and output

--- Song.py
+++ Song.py
@@ -4,24 +4,28 @@ import time
 
 class Song:
 
-    def __init__(self, bpm, corpus):
+    def __init__(self, bpm):
         self.notes = []
         self.bpm = bpm
 
-    def write_song(self):
-        mid = MidiFile()
+    def write_song(self, file_name):
+        mid = MidiFile(type=0) # 0 type means all messages are on one track
         track = MidiTrack()
         mid.tracks.append(track)
-        track.append(MetaMessage("set_tempo", mido.bpm2tempo(self.bpm)))
-        track.append(Message("note_on", note=60, time=50))
-        track.append(MetaMessage("end_of_track"))
+        track.append(MetaMessage("set_tempo", tempo=mido.bpm2tempo(self.bpm)))
+        track.append(Message('program_change', program=24, time=0))
+        track.append(Message("note_on", note=60, time=0))
+        track.append(Message("note_on", note=60, time=500, velocity=0))
+        track.append(Message("note_on", note=64, time=0))
 
-    def play(self, output):
-        outport = mido.open_output()
-        for note in self.notes:
-            outport.send(mido.Message("note_on", note=note.pitch, velocity=127))
-            time.sleep(note.duration)
-            outport.send(mido.Message("note_on", note=note.pitch, velocity=0))
+        mid.save(file_name)
+
+    def play(self, midi_file, port):
+        outport = mido.open_output(port)
+        for msg in MidiFile(midi_file):
+            time.sleep(msg.time)
+            if not msg.is_meta:
+                outport.send(msg)
 
     class Note:
 
@@ -29,8 +33,13 @@ class Song:
             self.duration = 0
             self.pitch = 0
 
-def __main__():
-    print("Running main!")
 
-if(__name__ == __main__):
+def __main__():
+    song = Song(240)
+    file = "test.mid"
+    song.write_song(file)
+    song.play(file, 'IAC Driver Bus 1')
+
+
+if __name__ == "__main__":
     __main__()
