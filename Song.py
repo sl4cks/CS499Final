329af598270c5bd5a611197c1c6277d5bffe3725
sl4cks <10481627+sl4cks@users.noreply.github.com>
2019-05-05 16:33:55 -0400

    arpeggio generator

--- Song.py
+++ Song.py
@@ -1,6 +1,7 @@
 import mido
 from mido import Message, MidiFile, MidiTrack, MetaMessage
 import time
+import random
 
 class Song:
 
@@ -9,14 +10,17 @@ class Song:
         self.bpm = bpm
 
     def write_song(self, file_name):
+        root = 60
+        arpeggio = [root, root+4, root+8, root+12]
         mid = MidiFile(type=0) # 0 type means all messages are on one track
         track = MidiTrack()
         mid.tracks.append(track)
         track.append(MetaMessage("set_tempo", tempo=mido.bpm2tempo(self.bpm)))
-        track.append(Message('program_change', program=24, time=0))
-        track.append(Message("note_on", note=60, time=0))
-        track.append(Message("note_on", note=60, time=500, velocity=0))
-        track.append(Message("note_on", note=64, time=0))
+
+        for i in range(200):
+            pitch = arpeggio[random.randint(0,3)]
+            track.append(Message("note_on", note=pitch, velocity=127, time=0))
+            track.append(Message("note_on", note=pitch, velocity=0, time=random.randint(1,4)*250))
 
         mid.save(file_name)
 
